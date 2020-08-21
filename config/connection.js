ed7798f7228a3a8421356c0c8cd0039c8829d771
nmcanall <nmcanall@citadel.edu>
2020-08-20 17:46:36 -0700

    add database connections

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const Sequelize = require("sequelize");
+require("dotenv").config(); // Pulls environment variables from '../.env'
+
+// Create connection to database; use MySQL; username and password parameters
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306
+});
+
+module.exports = sequelize;
