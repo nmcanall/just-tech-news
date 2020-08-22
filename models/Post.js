30bd150aac2293ac1d272c1b8ab78f4d7c99703c
nmcanall <nmcanall@citadel.edu>
2020-08-21 22:35:48 -0700

    create Post model and database

---
+++ models/Post.js
@@ -0,0 +1,45 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Create Post model
+class Post extends Model {}
+
+Post.init(
+    // Define columns of Post table
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id"
+            }
+        }
+    },
+
+    // Model configuration settings
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "post"
+    }
+);
+
+module.exports = Post;
