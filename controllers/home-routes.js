406345d8e38ca704c300827f8c46dcda3d19d7d9
nmcanall <nmcanall@citadel.edu>
2020-08-30 12:59:37 -0700

    add comment functions and handlebars condition statements

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,7 +29,7 @@ router.get("/", (req, res) => {
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({plain: true}));
             // Pass in a full object (array), then loop through that array in homepage.handlebars
-            res.render("homepage", {posts});
+            res.render("homepage", {posts, loggedIn: req.session.loggedIn});
         })
         .catch(err => {
             console.log(err);
@@ -82,7 +82,7 @@ router.get("/post/:id", (req, res) => {
             // Serialize the data
             const post = dbPostData.get({plain: true});
 
-            res.render("single-post", {post});
+            res.render("single-post", {post, loggedIn: req.session.loggedIn});
         })
         .catch(err => {
             console.log(error);
