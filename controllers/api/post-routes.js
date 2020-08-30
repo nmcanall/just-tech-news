f466373fe309b6a8eedebc681a8692260291f614
nmcanall <nmcanall@citadel.edu>
2020-08-30 12:25:24 -0700

    upvote function

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -91,13 +91,15 @@ router.post("/", (req, res) => {
 // PUT /api/posts/upvote
 router.put("/upvote", (req, res) => {
 
-    // Upvote function defined in "../../models/Post.js"
-    Post.upvote(req.body, {Vote})
-        .then(updatedPostData => res.json(updatedPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+    if(req.session) {
+        // Upvote function defined in "../../models/Post.js"
+        Post.upvote({...req.body, user_id: req.session.user_id}, {Vote, Comment, User})
+            .then(updatedPostData => res.json(updatedPostData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
 });
 
 // PUT /api/posts/id
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -47,9 +47,47 @@ router.get("/login", (req, res) => {
     res.render("login");
 })
 
-// Console log the session variable
-router.get("/", (req, res) => {
-    console.log(req.session);
+// View screen for single post at /post/:id
+router.get("/post/:id", (req, res) => {
+    Post.findOne({
+        where: {id: req.params.id},
+        attributes: [
+            "id", 
+            "post_url", 
+            "title", 
+            "created_at",
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ], 
+        include: [
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if(!dbPostData) {
+                res.status(404).json({message: "No post found with this ID"});
+                return;
+            }
+
+            // Serialize the data
+            const post = dbPostData.get({plain: true});
+
+            res.render("single-post", {post});
+        })
+        .catch(err => {
+            console.log(error);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
